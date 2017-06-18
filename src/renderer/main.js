9133c661acf85c1c23f934013b36de6b6a3f53da
sqlwwx <wwx_2012@hotmail.com>
2017-06-18 10:31:42 +0800

    add debug

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,11 +5,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+const debug = require('debug')('ws-admin-desktop:main')
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+debug('startApp')
 new Vue({
   components: { App },
   router,
